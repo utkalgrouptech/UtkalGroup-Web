72e26f889cb128608dddacc03e2416cec6b517b2
shubhashree500 <shreeshubha98@gmail.com>
2025-12-06 11:07:40 +0530

    google

--- src/Layouts/MainLayouts.tsx
+++ src/Layouts/MainLayouts.tsx
@@ -4,7 +4,6 @@ import Footer from "./Footer";
 import ResponsiveNavBar from "./Responsivenav";
 import Head from "next/head";
 
-
 type Prop = {
   title?: string;
   children: ReactElement | ReactElement[];
@@ -14,15 +13,27 @@ export default function MainLayouts({ children }: Prop) {
   return (
     <>
       <Head>
-      <link rel="icon" href="./logo.png "></link> 
-    </Head>
+        <link rel="icon" href="./logo.png" />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53FKQLZV');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
+
       <header className="sticky top-0 z-50 bg-white shadow-md"> 
         <Header />
       </header>
 
       <ResponsiveNavBar />
       <main>{children}</main>
-     
       <Footer />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53FKQLZV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Main />
         <NextScript />
       </body>
